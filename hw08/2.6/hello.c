20a5dd52007976f9b63277c154f4e4e31d7405c2
Demo User <debian@beaglebone.localdomain>
2019-10-31 16:48:15 +0000

    fixed bone

---
+++ hw08/2.6/hello.c
@@ -0,0 +1,32 @@
+#include <stdint.h>
+#include <pru_cfg.h>
+#include "resource_table_empty.h"
+
+#define GPIO1 0x4804C000
+#define GPIO_CLEARDATAOUT 0x190
+#define GPIO_SETDATAOUT 0x194
+#define USR0 (1<<21)
+#define USR1 (1<<22)
+#define USR2 (1<<23)
+#define USR3 (1<<24)
+unsigned int volatile * const GPIO1_CLEAR = (unsigned int ) (GPIO1 + GPIO_CLEARDATAOUT);
+unsigned int volatile * const GPIO1_SET   = (unsigned int *) (GPIO1 + GPIO_SETDATAOUT);
+
+volatile register unsigned int R30;
+volatile register unsigned int R31;
+
+void main(void) {
+    int i;
+
+    / Clear SYSCFG[STANDBY_INIT] to enable OCP master port */
+    CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
+
+    for(i=0; i<5; i++) {
+        *GPIO1_SET = USR3;      // The the USR3 LED on
+        delay_cycles(500000000/5);    // Wait 1/2 second
+
+        *GPIO1_CLEAR = USR3;
+         delay_cycles(500000000/5);
+    }
+    __halt();
+}
---
+++ hw08/ReadMe.md
@@ -0,0 +1,19 @@
+2.6 Blinking an LED
+    
+    
+    
+5.3 PWM Generator
+
+
+
+
+5.4 Controlling the PWM Frequency
+
+
+
+5.9 Reading the Input at Regular Intervals
+
+
+
+
+5.10 Analog Wave Generator
